752c476b4bc4d6356b98dfd9805f44598e653f54
duongph <duongph@smartosc.com>
2022-10-20 09:58:41 +0700

    duongph130599

--- src/main/java/com/ezbytes/cards/controller/CardsController.java
+++ src/main/java/com/ezbytes/cards/controller/CardsController.java
@@ -1,9 +1,15 @@
 package com.ezbytes.cards.controller;
 
+import com.ezbytes.cards.config.CardServiceConfig;
 import com.ezbytes.cards.entity.CardEntity;
 import com.ezbytes.cards.entity.Customer;
+import com.ezbytes.cards.entity.Properties;
 import com.ezbytes.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,9 @@ public class CardsController {
     @Autowired
     private CardsRepository cardsRepository;
 
+    @Autowired
+    private CardServiceConfig cardServiceConfig;
+
     @PostMapping("/myCards")
     public List<CardEntity> getCardDetails(@RequestBody Customer customer) {
         List<CardEntity> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
@@ -24,6 +33,17 @@ public class CardsController {
         }
 
         return null;
+    }
+
+    @GetMapping("/card/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardServiceConfig.getMsg(),
+                cardServiceConfig.getBuildVersion(),
+                cardServiceConfig.getMailDetails(),
+                cardServiceConfig.getActiveBranches()
+        );
+        return ow.writeValueAsString(properties);
     }
 
 
